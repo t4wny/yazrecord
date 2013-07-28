39d6f5fd03d6cdcc69fee8dc0e4645ea15880a07
Tawny Tidwell <t4wnyt@gmail.com>
2013-07-28 16:37:59 -0500

    Baby's First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yazrecord::Application.config.secret_key_base = 'ed625003fd55b5c01c6f4b8caba8ff5e1294a18261d7c3e565d77b8ca23e28feb1b6afd247fdf6ee6dcc26a89edc837b30c73a827eab8da38f91150e8268f7a1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yazrecord::Application.config.session_store :cookie_store, key: '_yazrecord_session'
